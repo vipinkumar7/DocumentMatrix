0bca1be414d83b60c0f3f63b8f0bfdd22ecda106
vipinkumar7 <vipinkumar.work@gmail.com>
2015-04-09 04:32:45 +0530

    added initial fils with code template and formatter

---
+++ documentmatrix/src/main/java/com/documentmatrix/controller/ReadController.java
@@ -0,0 +1,18 @@
+
+package com.documentmatrix.controller;
+
+/**
+ * 
+ * @author Vipin Kumar
+ * @created 09-Apr-2015
+ * 
+ * TODO: Write a quick description of what the class is supposed to do.
+ *
+ */
+
+
+public class ReadController
+{
+
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger( ReadController.class );
+}
