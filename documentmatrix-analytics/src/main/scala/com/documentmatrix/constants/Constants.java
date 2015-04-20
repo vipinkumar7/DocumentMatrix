22c88882b4babe83cf36fe12cb2dd1028397b213
root <root@hadoopdev01.ev.com>
2015-04-20 21:47:22 +0530

    added java and scala maven in pom

--- documentmatrix-analytics/src/main/scala/com/documentmatrix/constants/Constants.java
+++ documentmatrix-analytics/src/main/scala/com/documentmatrix/constants/Constants.java
@@ -3,7 +3,7 @@ package com.documentmatrix.constants;
 /**
  * Created by Vipin Kumar on 19/4/15.
  */
-public class Constants {
 
+public class Constants {
     public static final String HADOOP_HOST="hadoopdev01.ev.com";
 }
